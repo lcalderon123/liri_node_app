401b588b6601479b39d5d1a714c697a9d689e824
lcalderon123 <rockstarpoet321@gmail.com>
2017-12-13 16:02:35 -0800

    completed

---
+++ keys.js
@@ -0,0 +1,13 @@
+
+
+exports.twitterKeys = {
+  consumer_key: 'CeiBXX6T8LFF2wUiy6OLm0jUZ',
+  consumer_secret: 'qEtffNgPQ6AyuQty7FVmgibLuMASiAAN0MlWekQDNl8q3JQJ7y',
+  access_token_key: '940682458495795200-r6qot4jVE1lmPW5WrbyD82gI1t5dDRA',
+  access_token_secret: 'xD9PgApC1XvdjuvHMsa4RQXWD7qNz8d1kKermI99zVjkx',
+}
+
+exports.spotifyKeys = {
+  id: 'd7716ae0e23e4ab9b4889ef0987dc8d1',
+  secret: '08b459bfbf85467ea03e34715a95cc1f',
+}
